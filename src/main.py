f8cedd89c72813a4a710257e79ee8dbc349d96e3
31415us <pius.vondaeniken@epfl.ch>
2014-05-14 23:18:29 +0200

    dont compute trajectory directly -> request from server on same computer

--- src/main.py
+++ src/main.py
@@ -1,15 +1,21 @@
 
 import pygame,sys
+import json
+import socket
 
 from Circle import Circle
 from Vec2D import Vec2D
 import Globals
 from Environment import Environment,Obstacle
 
+import json
+import socket
+
 #pygame setup and vars
 px_per_meter = 400 
 width = Globals.PLAYGROUND_WIDTH * px_per_meter
 height = Globals.PLAYGROUND_HEIGHT * px_per_meter
+background = pygame.image.load("../demo/background.png")
 pygame.init()
 screen = pygame.display.set_mode((width,height))
 white = (255,255,255)
@@ -32,16 +38,11 @@ def main():
 
     circles = [c1,c2,c3,c4]
 
-    #env = Environment([Obstacle(c5,null),Obstacle(c6,null)])
-    #env = Environment([Obstacle(c1,Vec2D(-0.0001,0)),Obstacle(c2,Vec2D(0,-0.0001)),Obstacle(c3,Vec2D(0.0001,0)),Obstacle(c4,Vec2D(0,0.0001))])
-
-    env = Environment([Obstacle(c1,Vec2D(0.0000,0.0001)),Obstacle(c4,Vec2D(0,-0.0001))])
-    #env = Environment([Obstacle(c1,Vec2D(0,-0.0001))])
-
     start = Vec2D(0.01,0.01)
     end = Vec2D(2.99,1.99)
 
-    path = env.path(start,null,end,null,0.1)
+    obstacles = []
+
 
     #clock = pygame.time.Clock()
 
@@ -58,14 +59,48 @@ def main():
         if not paused:
 
             screen.fill(black)
+            screen.blit(background,(0,0))
 
-            env.update()
-            draw_env(env,start,end)
-            draw_path(env.path(start,null,end,null,0.1))
+            s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            s.connect(('127.0.0.1',1337))
+            s.send(json.dumps(to_request(start,null,end,null,0.1,1000,obstacles)))
+
+            traj = traj_from_response(json.loads(s.recv(4096).strip()))
+
+            s.close()
+
+            draw_path(traj)
+
 
             #print clock.tick()
 
             pygame.display.update()
+
+def to_request(start,v_start,end,v_end,delta_t,n,obstacles):
+    start_list = [int(start.x*1000),int(start.y*1000),int(v_start.x*1000),int(v_start.y*1000)]
+    end_list = [int(end.x*1000),int(end.y*1000)]
+    dt = int(delta_t*1000)
+    N = n
+    obstacle_list = []
+    for o in obstacles:
+        x = o.circle.pos.x
+        y = o.circle.pos.y
+        vx = o.v.x
+        vy = o.v.y
+        r = o.circle.r
+        obstacle_list.append([int(x*1000),int(x*1000),int(vx*1000),int(vy*1000),int(r*1000)])
+    return [start_list,end_list,dt,N,obstacle_list]
+
+def traj_from_response(response):
+    traj = []
+    for w in response:
+        x = float(w[0])/1000
+        y = float(w[1])/1000
+        vx = float(w[2])/1000
+        vy = float(w[3])/1000
+        t = float(w[4])/1000
+        traj.append((Vec2D(x,y),Vec2D(vx,vy),t))
+    return traj
 
 
 def draw_circle(c):
